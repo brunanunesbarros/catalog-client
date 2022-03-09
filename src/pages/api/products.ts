d971ecc39dde11c6cab47020bf75684fe4ba4e71
Bruna Barros <brunamary99@gmail.com>
2022-03-09 14:23:32 -0300

    show products by user

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -12,14 +12,22 @@ type Product = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Product[]>
+  res: NextApiResponse<Product[] | { message: string }>
 ) {
+
+  const { userid } = req.headers;
+
+  if (!userid) {
+    return res
+      .status(401)
+      .json({ message: 'User not present.' })
+  }
+
   const products: { data: any[] } = await fauna.query(
     q.Map(
-      q.Paginate(q.Documents(q.Collection('products')), { size: 10 }),
+      q.Paginate(q.Match(q.Index("products_by_owner"), q.Ref(q.Collection("users"), userid))),
       q.Lambda((x) => q.Get(x))
     )
-    
   );
 
   res.status(200).json(products.data)
--- src/pages/listAllProducts.tsx
+++ src/pages/listAllProducts.tsx
@@ -13,7 +13,7 @@ import {
     TableCaption,
     Tag,
     TagLabel,
-    Text,
+    Image,
     Checkbox,
     Badge,
     Drawer,
@@ -29,28 +29,52 @@ import {
     useDisclosure,
 } from "@chakra-ui/react";
 import { parseCookies } from "nookies";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FiLogOut } from "react-icons/fi";
 import { GrAdd } from "react-icons/gr";
 import { AuthContext } from "../contexts/authProvider";
+import { api } from "../services/api";
+import { Product } from "../types/product";
 
 export default function ListAllProducts() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { signOut, user } = useContext(AuthContext);
+    const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        
         const { "catalog-client": token } = parseCookies();
         if (!token) {
             signOut();
         }
     }, [signOut]);
 
+    useEffect(() => {
+        if (user) {
+            api.get("/api/products", {
+                headers: {
+                    userId: user?.id as string,
+                },
+            })
+                .then((result) => {
+                    const productList = result.data.map((item: any) => {
+                        return {
+                            ...item.data,
+                            id: item.ref["@ref"].id,
+                        };
+                    });
+                    setProducts(productList);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, [user]);
+
     return (
         <Flex
             flexDirection="column"
             width={"100wh"}
-            height="100vh"
+            minHeight="100vh"
             backgroundColor="gray.300"
         >
             <HStack
@@ -180,58 +204,45 @@ export default function ListAllProducts() {
                         bg="blue.600"
                         color="white"
                     >
-                        Total de produtos: 4{" "}
+                        Total de produtos: {products.length}
                     </Badge>
                 </TableCaption>
                 <Thead>
                     <Tr>
                         <Th></Th>
+                        <Th></Th>
                         <Th>Produto</Th>
                         <Th>Descrição</Th>
-                        <Th>Quantidade</Th>
                         <Th>Preço</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
-                    <Tr>
-                        <Td>
-                            <Checkbox />
-                        </Td>
-                        <Td>Produto 01</Td>
-                        <Td>Essa é uma descrição ficticia</Td>
-                        <Td>2 unidades</Td>
-                        <Td>R$ 30,00</Td>
-                    </Tr>
-
-                    <Tr>
-                        <Td>
-                            <Checkbox />
-                        </Td>
-                        <Td>Produto 01</Td>
-                        <Td>Essa é uma descrição ficticia</Td>
-                        <Td>2 unidades</Td>
-                        <Td>R$ 30,00</Td>
-                    </Tr>
-
-                    <Tr>
-                        <Td>
-                            <Checkbox />
-                        </Td>
-                        <Td>Produto 01</Td>
-                        <Td>Essa é uma descrição ficticia</Td>
-                        <Td>2 unidades</Td>
-                        <Td>R$ 30,00</Td>
-                    </Tr>
-
-                    <Tr>
-                        <Td>
-                            <Checkbox />
-                        </Td>
-                        <Td>Produto 01</Td>
-                        <Td>Essa é uma descrição ficticia</Td>
-                        <Td>2 unidades</Td>
-                        <Td>R$ 30,00</Td>
-                    </Tr>
+                    {products.map((product) => {
+                        return (
+                            <Tr key={product.id}>
+                                <Td>
+                                    <Checkbox />
+                                </Td>
+                                <Td>
+                                    <Image
+                                        src={product.image}
+                                        alt={product.name}
+                                        width="100px"
+                                        height="60px"
+                                        objectFit="cover"
+                                    />
+                                </Td>
+                                <Td>{product.name}</Td>
+                                <Td>{product.description}</Td>
+                                <Td>
+                                    {product.price.toLocaleString("pt-BR", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    })}
+                                </Td>
+                            </Tr>
+                        );
+                    })}
                 </Tbody>
             </Table>
         </Flex>
